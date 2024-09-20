8182fee284fa2b609095e7c52088d1e007df13d5
Alina <77487060+Alya-Homyak@users.noreply.github.com>
2024-09-20 12:09:14 +0200

    Добавляет адаптивные сетки (#23)
    * Добавляет адаптивную сетку секции навигации
    
    * Добавляет адаптивную сетку секции навигации странице каталога
    
    * Добавляет адаптивные сетки для хиро
    
    * Добавляет адаптивные сетки каталога

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const navButton = document.querySelector('.header__nav-button');
+const navSection = document.querySelectorAll('.header__nav');
+
+navButton.onClick = function() {
+  if (navButton.classList.contains('header__nav-button--open')) {
+    navSection.classList.remove('visually-hidden');
+    navButton.classList.add('header__nav-button--close');
+    navButton.classList.remove('header__nav-button--open');
+  } else {
+    navSection.classList.add('visually-hidden');
+    navButton.classList.remove('header__nav-button--close');
+    navButton.classList.add('header__nav-button--open');
+  }
+};
+
+
